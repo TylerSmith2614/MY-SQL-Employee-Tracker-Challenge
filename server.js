6453a6203754a6bf463981f4455adc0f3de0ce71
tylersmith2614 <tylersmith2614@gmail.com>
2023-05-15 19:03:54 -0600

    added functions for the rest of the options in the application

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ function init() {
       choices: [
         "View all departments",
         "View all roles",
-        // will need to join tables together. join on the foreign key.
         "View all employees",
-        // join the table on itself. selfjoin.
         "Add a department",
         "Add a role",
         "Add an employee",
@@ -75,6 +73,91 @@ async function viewAllDepartments() {
   try {
     const res = await db.query("SELECT * FROM department");
     console.table(res);
+    init();
+  } catch (error) {
+    console.log(error);
+  }
+}
+// const query = "SELECT roles.title, roles.id, departments.department_name, roles.salary from roles join departments on roles.department_id = departments.id";
+async function viewAllRoles() {
+  try {
+    const res = await db.query(
+      "SELECT roles.title, roles.id, department.department_name, roles.salary FROM roles join department on roles.department_id = department.id"
+    );
+    console.table(res);
+    init();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function viewAllEmployees() {
+  try {
+    const res = await db.query(
+      ` SELECT a.id, a.first_name, a.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+      FROM employee a
+      LEFT JOIN roles r ON a.role_id = r.id
+      LEFT JOIN department d ON r.department_id = d.id
+      LEFT JOIN employee m ON a.manager_id = m.id;
+    `
+    );
+    console.table(res);
+    init();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addDepartment() {
+  try {
+    const answer = await inquirer.prompt([
+      {
+        type: "input",
+        name: "DepartmentName",
+        message: "What is the department name you want to add?",
+      },
+    ]);
+    await db.query("INSERT INTO department (department_name) VALUES (?)", [
+      answer.DepartmentName,
+    ]);
+    console.log("Your new department was added.");
+    init();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addRole() {
+  // WHEN I choose to add a role
+  // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the databaseasync function addRole() {
+  try {
+    const department = await db.query(
+      "Select id as value, department_name as name FROM department"
+    );
+    const answer = await inquirer.prompt([
+      {
+        type: "input",
+        name: "Title",
+        message: "What is the title of the role you would like to add",
+      },
+      {
+        type: "input",
+        name: "Salary",
+        message: "What is the salary for this role",
+      },
+      {
+        type: "list",
+        name: "Department",
+        message: "What department does the role belong to?",
+        choices: department,
+      },
+    ]);
+    await db.query(
+      "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
+      [answer.Title, answer.Salary, answer.Department]
+    );
+    console.log("Your new role was added.");
+    init();
   } catch (error) {
     console.log(error);
   }
